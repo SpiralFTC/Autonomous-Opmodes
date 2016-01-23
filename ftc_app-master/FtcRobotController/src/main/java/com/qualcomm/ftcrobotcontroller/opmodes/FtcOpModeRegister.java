6a16b21ebafbad4e293889f98cd16498902b926a
Chaithu1 <chaithudikkala@gmail.com>
2016-01-23 14:32:23 -0800

    Merge branch 'master' of https://github.com/SpiralFTC/Autonomous-Opmodes into 2Choo
    # Conflicts:
    #	ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
    #	ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BlueFarSideAutonomous.java
    #	ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
    #	ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Gyro_Programs.java
    #	ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Methods.java

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -33,13 +33,13 @@ import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
  */
 public class FtcOpModeRegister implements OpModeRegister {
 
-  /**
-   * The Op Mode Manager will call this method when it wants a list of all
-   * available op modes. Add your op mode to the list to enable it.
-   *
-   * @param manager op mode manager
-   */
-  public void register(OpModeManager manager) {
+    /**
+     * The Op Mode Manager will call this method when it wants a list of all
+     * available op modes. Add your op mode to the list to enable it.
+     *
+     * @param manager op mode manager
+     */
+    public void register(OpModeManager manager) {
 
     /*
      * register your op modes here.
@@ -66,38 +66,39 @@ public class FtcOpModeRegister implements OpModeRegister {
      * Uncomment any of the following lines if you want to register an op mode.
      */
 
-    //manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
-    //manager.register("ColorSensorDriver", ColorSensorDriver.class);
+        //manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
+        //manager.register("ColorSensorDriver", ColorSensorDriver.class);
 
-    //manager.register("IrSeekerOp", IrSeekerOp.class);
-    //manager.register("CompassCalibration", CompassCalibration.class);
-    //manager.register("I2cAddressChangeExample", LinearI2cAddressChange.class);
-
-
-    //manager.register("NxtTeleOp", NxtTeleOp.class);
-
-    //manager.register("LinearK9TeleOp", LinearK9TeleOp.class);
-    //manager.register("LinearIrExample", LinearIrExample.class);
-
-    //manager.register ("PushBotManual1", PushBotManual1.class);
-    //manager.register ("PushBotAutoSensors", PushBotAutoSensors.class);
-    //manager.register ("PushBotIrEvent", PushBotIrEvent.class);
-
-    //manager.register ("PushBotManualSensors", PushBotManualSensors.class);
-    //manager.register ("PushBotOdsDetectEvent", PushBotOdsDetectEvent.class);
-    //manager.register ("PushBotOdsFollowEvent", PushBotOdsFollowEvent.class);
-    //manager.register ("PushBotTouchEvent", PushBotTouchEvent.class);
-
-    //manager.register("PushBotDriveTouch", PushBotDriveTouch.java);
-    //manager.register("PushBotIrSeek", PushBotIrSeek.java);
-    //manager.register("PushBotSquare", PushBotSquare.java);
-
-    manager.register("WheelTele-OP", TeleOp.class);
-    manager.register("Tankie, ITS OP", TankDrive.class);
-    manager.register("RedFarSideAuto", RedFarSideAutonomous.class);
-    manager.register("Auto ",Autonomous.class);
-    manager.register("BlueFarSideAuto",BlueFarSideAutonomous.class  );
+        //manager.register("IrSeekerOp", IrSeekerOp.class);
+        //manager.register("CompassCalibration", CompassCalibration.class);
+        //manager.register("I2cAddressChangeExample", LinearI2cAddressChange.class);
 
 
-  }
+        //manager.register("NxtTeleOp", NxtTeleOp.class);
+
+        //manager.register("LinearK9TeleOp", LinearK9TeleOp.class);
+        //manager.register("LinearIrExample", LinearIrExample.class);
+
+        //manager.register ("PushBotManual1", PushBotManual1.class);
+        //manager.register ("PushBotAutoSensors", PushBotAutoSensors.class);
+        //manager.register ("PushBotIrEvent", PushBotIrEvent.class);
+
+        //manager.register ("PushBotManualSensors", PushBotManualSensors.class);
+        //manager.register ("PushBotOdsDetectEvent", PushBotOdsDetectEvent.class);
+        //manager.register ("PushBotOdsFollowEvent", PushBotOdsFollowEvent.class);
+        //manager.register ("PushBotTouchEvent", PushBotTouchEvent.class);
+
+        //manager.register("PushBotDriveTouch", PushBotDriveTouch.java);
+        //manager.register("PushBotIrSeek", PushBotIrSeek.java);
+        //manager.register("PushBotSquare", PushBotSquare.java);
+
+        manager.register("BlueNearSideAuto", BeaconBlueOnly.class);
+        manager.register("RedNearSideAuto", BeaconRedOnly.class);
+        manager.register("Tankie, ITS OP", TankDrive.class);
+        manager.register("RedFarSideAuto", RedFarSideAutonomous.class);
+        manager.register("Auto ", Autonomous.class);
+        manager.register("BlueFarSideAuto", BlueFarSideAutonomous.class);
+
+
+    }
 }
--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/RedFarSideAutonomous.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/RedFarSideAutonomous.java
@@ -3,7 +3,6 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Rohith on 12/30/2015.
@@ -11,32 +10,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class RedFarSideAutonomous extends Methods {
 
 
-    @Override
-    public void init() {
-        climberServo = hardwareMap.servo.get("arm");
-        armLatchServo = hardwareMap.servo.get("armLatch");
-        ziplineServo = hardwareMap.servo.get("leftS");
-        gyroSensor = hardwareMap.gyroSensor.get("gyro");
-        gyroSensor.calibrate();
-        if (gyroSensor.isCalibrating()) {
-            sleep(400);
-        }
-        leftMotor = hardwareMap.dcMotor.get("left");
-        rightMotor = hardwareMap.dcMotor.get("right");
-        armLatchServo.setPosition(1);
-
-    }
-
-    @Override
-    public void start() {
-        super.start();
-
-        leftMotor.setDirection(DcMotor.Direction.REVERSE);
-        leftMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        rightMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
 
 
-    }
 
     @Override
     public void loop() {
@@ -168,8 +143,8 @@ public class RedFarSideAutonomous extends Methods {
                 //lift arm to drop climbers in beacon
 
                 //  ZiplineTriggerServoRight.setPosition(climberArmPosition);
-                ziplineServo.setPosition(climberArmPosition);
-                if (ziplineServo.getPosition() == climberArmPosition) {
+                ZiplineTriggerServoRight.setPosition(climberArmPosition);
+                if (ZiplineTriggerServoRight.getPosition() == climberArmPosition) {
                     climberArmPosition = 1;
                     state++;
                 }
@@ -180,8 +155,8 @@ public class RedFarSideAutonomous extends Methods {
             case 12:
                 //return arm to original position
 
-                ziplineServo.setPosition(climberArmPosition);
-                if (ziplineServo.getPosition() == climberArmPosition) {
+                ZiplineTriggerServoRight.setPosition(climberArmPosition);
+                if (ZiplineTriggerServoRight.getPosition() == climberArmPosition) {
                     climberArmPosition = 0;
                     state+= 2;
                 }
